f5bee26a59c550160358a48c058d0304524e147b
Marc Augier <m.augier@me.com>
2021-09-25 17:01:02 +0200

    step 2 avec annotations

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,11 +2,26 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class QuestionController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage ()
     {
         return new Response('What  betwitching controller');
     }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the question "%s"!',
+            ucwords(str_replace('-', ' ', $slug))
+        ));
+   }
 }
